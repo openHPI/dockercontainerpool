b8f53ce17b483f7a7c5cc0c32488889bb7146867
Sebastian Serth <Sebastian.Serth@hpi.de>
2021-10-31 01:03:52 +0200

    Change HTTP verbs

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   scope 'docker_container_pool' do
-    get 'get_container/:execution_environment_id', to: 'container_pool#get_container'
-    get 'return_container/:container_id', to: 'container_pool#return_container'
-    get 'destroy_container/:container_id', to: 'container_pool#destroy_container'
+    post 'get_container/:execution_environment_id', to: 'container_pool#get_container'
+    put 'return_container/:container_id', to: 'container_pool#return_container'
+    delete 'destroy_container/:container_id', to: 'container_pool#destroy_container'
     get 'quantities', to: 'container_pool#quantities'
     get 'dump_info', to: 'container_pool#dump_info'
     get 'available_images', to: 'container_pool#available_images'
